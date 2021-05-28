8485f3d9133d4e962f8e07f2e2d72ea521575036
Valentin Achimescu <50949589+Achime11@users.noreply.github.com>
2021-05-28 15:15:11 +0300

    Use Lambda Expression cases for switch and some checks for CNPs

--- src/servicii/Main.java
+++ src/servicii/Main.java
@@ -46,134 +46,155 @@ public class Main {
         Programari.sort(Programare::compareTo);
         Echipamente.sort(Echipament::compareTo);
         //Testare JDBC SERVICIES
-        Medic medic = new Medic();
 
 
+        Date data;
+        SimpleDateFormat formater = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        String timestamp;
         //main
         Scanner scan = new Scanner(System.in);
         int opt;
         do {
-            Date data = new Date();
-            SimpleDateFormat formater = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-            String timestamp = formater.format(data);
             initiziarizareMeniu();
             System.out.println();
             System.out.println();
             System.out.println("Optiunea aleasa: ");
             opt = scan.nextInt();
             switch (opt) {
-                case 1:
+                case 1 -> {
                     Client client = Servicii.adaugareClient();
-                    data = new Date();
-                    timestamp = formater.format(data);
-                    Servicii.adaugareAudit("Adaugare client",timestamp);
-                    Servicii.insertClient(client);
-                    Clienti.put(client.getCnp(), client);
-                    System.out.println("Client adaugat!");
-                    break;
-                case 2:
+                    if (!Clienti.containsKey(client.getCnp())) {
+                        data = new Date();
+                        timestamp = formater.format(data);
+                        Servicii.adaugareAudit("Adaugare client", timestamp);
+                        Servicii.insertClient(client);
+                        Clienti.put(client.getCnp(), client);
+                        System.out.println("Client adaugat!");
+                    } else System.out.println("Exista deja un client cu acest CNP!!!");
+                }
+                case 2 -> {
                     System.out.println("Afisare Clienti");
                     for (Map.Entry x : Clienti.entrySet())
                         Servicii.afisareClient((Client) x.getValue());
                     data = new Date();
                     timestamp = formater.format(data);
-                    Servicii.adaugareAudit("Afisare clienti",timestamp);
-                    break;
-                case 3:
+                    Servicii.adaugareAudit("Afisare clienti", timestamp);
+                }
+                case 3 -> {
                     System.out.println("Modificare Client(Introduceti datele clientului)");
-
                     System.out.println("CNP: ");
                     String cnp = scan.next();
-
-                    if(Clienti.containsKey(cnp)){
+                    if (Clienti.containsKey(cnp)) {
                         Client a = Servicii.modificareClient();
                         Clienti.remove(cnp);
                         Clienti.put(a.getCnp(), a);
-                        Servicii.updateClient(cnp,a);
+                        Servicii.updateClient(cnp, a);
 
                         data = new Date();
                         timestamp = formater.format(data);
-                        Servicii.adaugareAudit("Modificare client",timestamp);
-                    }
-                    else System.out.println("Nu exista un client cu acest CNP!!!");
-
-                    break;
-                case 4:
+                        Servicii.adaugareAudit("Modificare client", timestamp);
+                    } else System.out.println("Nu exista un client cu acest CNP!!!");
+                }
+                case 4 -> {
                     System.out.println("Stergere Client(Introduceti datele clientului)");
                     System.out.println("CNP: ");
                     String cnp_stergere = scan.next();
-
-                    if(Clienti.containsKey(cnp_stergere)){
+                    if (Clienti.containsKey(cnp_stergere)) {
                         Clienti.remove(cnp_stergere);
                         Servicii.deletePersoana(cnp_stergere);
 
                         data = new Date();
                         timestamp = formater.format(data);
-                        Servicii.adaugareAudit("Stergere client",timestamp);
-                    }
-                    else System.out.println("Nu exista un client cu acest CNP!!!");
-
-
-                    break;
-                case 5:
-                    System.out.println("Statistica COVID!");
+                        Servicii.adaugareAudit("Stergere client", timestamp);
+                    } else System.out.println("Nu exista un client cu acest CNP!!!");
+                }
+                case 5 -> {
+                    System.out.println("--------Statistica COVID--------");
                     Servicii.statisticaCOVID(Clienti);
                     data = new Date();
                     timestamp = formater.format(data);
-                    Servicii.adaugareAudit("Statistica COVID",timestamp);
-                    break;
-                case 6:
+                    Servicii.adaugareAudit("Statistica COVID", timestamp);
+                }
+                case 6 -> {
                     System.out.println("Afisarea mediei de experienta a Medicilor");
                     Servicii.afisareMedieExperienta(Medici);
                     data = new Date();
                     timestamp = formater.format(data);
-                    Servicii.adaugareAudit("Afisarea mediei de experienta a Medicilor",timestamp);
-                    break;
-                case 7:
+                    Servicii.adaugareAudit("Afisarea mediei de experienta a Medicilor", timestamp);
+                }
+                case 7 -> {
                     System.out.println("Afisarea celui mai vechi medic!");
                     Servicii.afisareCelMaiVechiMedic(Medici);
                     data = new Date();
                     timestamp = formater.format(data);
-                    Servicii.adaugareAudit("Afisarea celui mai vechi medic",timestamp);
-                    break;
-                case 8:
+                    Servicii.adaugareAudit("Afisarea celui mai vechi medic", timestamp);
+                }
+                case 8 -> {
                     System.out.println("Date medici");
+                    Boolean stop_actions = false;
                     for (Map.Entry x : Medici.entrySet())
                         System.out.println(x.getValue().toString());
                     System.out.println("Adaugare programare");
                     System.out.println("CNP Client: ");
                     String cnp_client = scan.next();
+                    while (!Clienti.containsKey(cnp_client)){
+                        System.out.println("Nu exista niciun client cu acest CNP!");
+                        System.out.println("Introduceti CNP 0 daca doriti sa opriti actiunea de adaugare a unei programari!");
+                        System.out.println("CNP Client: ");
+                        cnp_client = scan.next();
+                        if(cnp_client.equals("0")){
+                            stop_actions = true;
+                            break;
+                        }
+                    }
+                    if(stop_actions)
+                        break;
                     System.out.println("CNP Medic: ");
                     String cnp_medic = scan.next();
+                    while (!Clienti.containsKey(cnp_medic)){
+                        System.out.println("Nu exista niciun medic cu acest CNP!");
+                        System.out.println("Introduceti CNP 0 daca doriti sa opriti actiunea de adaugare a unei programari!");
+                        System.out.println("CNP Medic: ");
+                        cnp_medic = scan.next();
+                        if(cnp_medic.equals("0")){
+                            stop_actions = true;
+                            break;
+                        }
+
+                    }
+                    if(stop_actions)
+                        break;
+
                     Programare aux = Servicii.adaugareProgramare(cnp_client, cnp_medic);
                     Servicii.insertProgramare(aux);
                     Programari.add(aux);
                     data = new Date();
                     timestamp = formater.format(data);
-                    Servicii.adaugareAudit("Adaugare programare",timestamp);
-                    break;
-                case 9:
+                    Servicii.adaugareAudit("Adaugare programare", timestamp);
+                }
+                case 9 -> {
                     System.out.println("Afisarea programarilor");
                     for (Programare it : Programari)
                         Servicii.afisareProgramare(it);
                     data = new Date();
                     timestamp = formater.format(data);
-                    Servicii.adaugareAudit("Afisarea programarilor",timestamp);
-                    break;
-                case 10:
+                    Servicii.adaugareAudit("Afisarea programarilor", timestamp);
+                }
+                case 10 -> {
                     System.out.println("Afisarea valori medii a echipamentelor!");
                     Servicii.afisareValoareaMediaEchipamente(Echipamente);
                     data = new Date();
                     timestamp = formater.format(data);
-                    Servicii.adaugareAudit("Afisarea valori medii a echipamentelor",timestamp);
-                    break;
-                case 0:
+                    Servicii.adaugareAudit("Afisarea valori medii a echipamentelor", timestamp);
+                }
+                case 0 -> {
                     Servicii.scriereDataBase(Clienti, Medici, Programari, Retete, Echipamente);
                     System.exit(0);
-                    break;
-                default:
+                }
+                default -> {
                     System.out.println("OPTIUNEA ALEASA ESTE INVALIDA!");
                     System.out.println("ALEGETI O OPTIUNE VALIDA!");
+                }
             }
         } while (opt != -1);
 
